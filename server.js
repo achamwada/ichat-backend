043fcc00e252bf901d078579d03f8b39bcaa3d33
Alexander Chamwada <alexanderchamwada@gmail.com>
2019-06-23 21:20:45 +0100

    Users and auth

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+// configure port
+const PORT = process.env.PORT || 5000;
+
+// Add middleware to process json requests
+app.use(express.json({ extended: false }));
+
+// API Routes
+app.use("/api/user", require('./routes/user/users'));
+
+// listen on port
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
